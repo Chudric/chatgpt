b2902e3f197c83e7157d604d6b21d266e1006f00
Zhichu Zheng <zzhen1993@outlook.com>
2023-04-28 23:42:32 +0800

    Update generate.ts

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -1,49 +1,58 @@
-// #vercel-disable-blocks
-import { ProxyAgent, fetch } from 'undici'
-// #vercel-end
-import { generatePayload, parseOpenAIStream } from '@/utils/openAI'
-import { verifySignature } from '@/utils/auth'
 import type { APIRoute } from 'astro'
 
 const apiKey = import.meta.env.OPENAI_API_KEY
-const httpsProxy = import.meta.env.HTTPS_PROXY
-const baseUrl = ((import.meta.env.OPENAI_API_BASE_URL) || 'https://api.openai.com').trim().replace(/\/$/, '')
-const sitePassword = import.meta.env.SITE_PASSWORD || ''
-const passList = sitePassword.split(',') || []
+const baseUrl = "https://api.writesonic.com/v2/business/content/chatsonic?engine=premium&language=zh"
 
-export const post: APIRoute = async(context) => {
-  const body = await context.request.json()
-  const { sign, time, messages, pass } = body
-  if (!messages) {
-    return new Response(JSON.stringify({
-      error: {
-        message: 'No input text.',
-      },
-    }), { status: 400 })
-  }
-  if (sitePassword && !(sitePassword === pass || passList.includes(pass))) {
-    return new Response(JSON.stringify({
-      error: {
-        message: 'Invalid password.',
-      },
-    }), { status: 401 })
-  }
-  if (import.meta.env.PROD && !await verifySignature({ t: time, m: messages?.[messages.length - 1]?.content || '' }, sign)) {
-    return new Response(JSON.stringify({
-      error: {
-        message: 'Invalid signature.',
-      },
-    }), { status: 401 })
-  }
-  const initOptions = generatePayload(apiKey, messages)
-  // #vercel-disable-blocks
-  if (httpsProxy)
-    initOptions.dispatcher = new ProxyAgent(httpsProxy)
-  // #vercel-end
+// HTML 转换为 Markdown 的函数
+function htmlToMarkdown(html) {
+  let markdown = html
+  // 处理换行标签
+  markdown = markdown.replace(/<br\s*\/?>/gi, '\n')
+  // 处理粗体标签
+  markdown = markdown.replace(/<b>(.*?)<\/b>/gi, '**$1**')
+  // 处理超链接标签
+  markdown = markdown.replace(/<span>\[(\d+)\]\s*<a href="(.*?)"[^>]*class='text-purple-1 underline'>(.*?)<\/a><\/span>/gi, (match, index, url, text) => `[${index}][${text}](${url})`)
+  // 移除其它 HTML 标签
+  markdown = markdown.replace(/<\/?[^>]+(>|$)/g, '')
 
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-expect-error
-  const response = await fetch(`${baseUrl}/v1/chat/completions`, initOptions).catch((err: Error) => {
+  return markdown
+}
+
+export const post: APIRoute = async (context) => {
+  try {
+    const body = await context.request.json()
+    const { messages } = body
+    if (!messages) {
+      return new Response(JSON.stringify({
+        error: {
+          message: 'No input text.',
+        },
+      }), { status: 400 })
+    }
+    const input_text = messages[messages.length - 1].content
+    const response = await fetch(baseUrl, {
+      headers: {
+        'Content-Type': 'application/json',
+        'X-API-KEY': apiKey,
+      },
+      method: 'POST',
+      body: JSON.stringify({
+        enable_google_results: "true",
+        enable_memory: true,
+        input_text
+      }),
+    })
+
+    // 解析响应并提取 "message" 字段
+    const responseData = await response.json()
+    const messageContent = responseData.message
+
+    // 将 HTML 转换为 Markdown
+    const markdownContent = htmlToMarkdown(messageContent)
+
+    // 返回 Markdown 格式的 "message" 字段的值
+    return new Response(markdownContent, { status: response.status })
+  } catch (err) {
     console.error(err)
     return new Response(JSON.stringify({
       error: {
@@ -51,7 +60,5 @@ export const post: APIRoute = async(context) => {
         message: err.message,
       },
     }), { status: 500 })
-  }) as Response
-
-  return parseOpenAIStream(response) as Response
+  }
 }
